63ea8131e349eafeb8ebc948dc0fec2fe029aa01
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-24 17:52:29 +0800

    copy exsiting pylsp configs

--- lua/plugins/mic.lua
+++ lua/plugins/mic.lua
@@ -19,4 +19,54 @@ return {
       },
     },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                flake8 = {
+                  enabled = true,
+                  maxLineLength = 120,
+                },
+                pycodestyle = {
+                  enabled = false, -- Included in flake8
+                  maxLineLength = 120,
+                },
+                mccabe = {
+                  enabled = false, -- Included in flake8
+                },
+                pyflakes = {
+                  enabled = false, -- Included in flake8
+                },
+                pydocstyle = {
+                  enabled = true,
+                  convention = "google",
+                },
+                pylint = {
+                  enabled = true,
+                  args = {
+                    "--max-line-length",
+                    "120",
+                  },
+                },
+                rope_autoimport = {
+                  enabled = false,
+                },
+                rope_completion = {
+                  enabled = true,
+                  eager = true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
