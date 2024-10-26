fda447315ca5d81c6449f872626e25bdec93c7f3
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-26 20:13:00 +0800

    move extras into correct place

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,14 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/mic.lua
+++ lua/plugins/mic.lua
@@ -1,13 +1,4 @@
 return {
-  -- Install lazy extras
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.editor.telescope" },
-  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
-  { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.lang.sql" },
 
   -- Install mason packs
   {
