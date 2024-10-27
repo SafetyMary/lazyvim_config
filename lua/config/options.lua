9b2bd43fdae5f434bf2fca595ed2c86ad0ed21ee
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-27 11:47:10 +0800

    add folding sign

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,11 @@
 -- Add any additional options here
 
 vim.g.autoformat = false
+
+-- Options for the LazyVim statuscolumn
+vim.g.lazyvim_statuscolumn = {
+  folds_open = true, -- show fold sign when fold is open
+  folds_githl = true, -- highlight fold sign with git sign color
+}
+
 vim.g.lazyvim_prettier_needs_config = false
