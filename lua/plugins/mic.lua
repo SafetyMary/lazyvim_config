7c00231c5f6ee0ab9d63cc9e92622fb141fe961e
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-27 17:09:44 +0800

    add boarders to hover windows

--- lua/plugins/mic.lua
+++ lua/plugins/mic.lua
@@ -115,4 +115,32 @@ return {
       })
     end,
   },
+
+  -- window borders (https://github.com/LazyVim/LazyVim/issues/2708)
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
 }
