67d49c19d8fb2a27409bd0cace7999d704209621
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-27 14:49:03 +0800

    move extras back to plugins

--- lua/plugins/mic.lua
+++ lua/plugins/mic.lua
@@ -1,5 +1,16 @@
 return {
 
+  -- Add extras
+  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+
   -- Install mason packs
   {
     "williamboman/mason.nvim",
