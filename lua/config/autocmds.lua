f4ab3ca5a63941ed534db6a3d2906edfe2980f18
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-10-27 11:55:58 +0800

    move colorcolumn autocmd and spellcheck autocmd to all filetype events

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,11 +2,23 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Run some neovim commands after setup
-vim.api.nvim_create_autocmd("SafeState", {
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Set a 120 character color column visual guide
+vim.api.nvim_create_autocmd("FileType", {
   callback = function()
-    vim.api.nvim_set_option_value("colorcolumn", "120", {}) -- TODO: disable this in front page
-    vim.cmd.setlocal({ "spell", "spelllang=en_us" })
+    vim.api.nvim_set_option_value("colorcolumn", "120", {})
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
   end,
 })
 
