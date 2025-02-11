651a6150d1cfc768303ce19ad8c1690222490716
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2025-02-11 10:56:45 +0800

    set python3 host prog, have dedicated venv for neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,9 @@ vim.g.lazyvim_statuscolumn = {
 
 vim.g.lazyvim_prettier_needs_config = false
 
+-- have dedicated venv for hosting python with neovim package
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/venv/bin/python3")
+
 local opt = vim.opt
 
 opt.autowrite = false -- disable auto saving
