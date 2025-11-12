ee2c0309ecb0c89a9acf1a803268d7bf7224acc9
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2025-11-13 00:05:02 +0800

    allow more than 20 public methods in a class

--- lua/plugins/mic.lua
+++ lua/plugins/mic.lua
@@ -199,6 +199,7 @@ return {
                   "DOC201", -- ignore docstring return statement check
                   "S101", -- Allow use of assert
                   "PLR0913", -- Allow more than 5 args in a function
+                  "PLR0904", -- Allow more than 20 public methods in a class
                 },
               },
             },
